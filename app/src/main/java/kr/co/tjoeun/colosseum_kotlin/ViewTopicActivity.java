8e8daa4638762af9bb73674b8c90e77752d5699f
dy696786 <dy696786@gmail.com>
2020-06-03 09:58:11 +0900

    주제 제목 / 투표 현황 표기 XML그리기

---
+++ app/src/main/java/kr/co/tjoeun/colosseum_kotlin/ViewTopicActivity.java
@@ -0,0 +1,28 @@
+package kr.co.tjoeun.colosseum_kotlin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewTopicActivity extends BaseActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_topic);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
