ed3e9594384debd2533c41dd46887095d5b9b249
dy696786 <dy696786@gmail.com>
2020-06-03 10:19:26 +0900

    주제의 이미지 / 제목을 실제 반영

--- app/src/main/java/kr/co/tjoeun/colosseum_kotlin/ViewTopicActivity.java
+++ app/src/main/java/kr/co/tjoeun/colosseum_kotlin/ViewTopicActivity.java
@@ -7,14 +7,20 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import kr.co.tjoeun.colosseum_kotlin.databinding.ActivityViewTopicBinding;
+import kr.co.tjoeun.colosseum_kotlin.datas.Topic;
 import kr.co.tjoeun.colosseum_kotlin.utils.ServerUtil;
 
 public class ViewTopicActivity extends BaseActivity {
 
     ActivityViewTopicBinding binding ;
+
+    Topic mTopic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +50,31 @@ public class ViewTopicActivity extends BaseActivity {
             @Override
             public void onResponse(JSONObject json) {
                 Log.d("토픽 상세 정보", json.toString());
+
+                try {
+                    JSONObject data = json.getJSONObject("data");
+                    JSONObject topic = data.getJSONObject("topic");
+
+                    mTopic = Topic.getTopicFromJson(topic);
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            setTopicvaluesToUi();
+                        }
+                    });
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
         
+    }
+
+    void setTopicvaluesToUi(){
+        binding.topicTitleTxt.setText(mTopic.getTitle());
+        Glide.with(mContext).load(mTopic.getImageUrl()).into(binding.topicImg);
+
     }
 }
