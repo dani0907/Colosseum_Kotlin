26ca8453febbb64bc99ac605b1999e7018dff75f
dy696786 <dy696786@gmail.com>
2020-06-03 10:14:30 +0900

    주제 id만 전달받아서 상세정보를 서버에서 재확인

--- app/src/main/java/kr/co/tjoeun/colosseum_kotlin/ViewTopicActivity.java
+++ app/src/main/java/kr/co/tjoeun/colosseum_kotlin/ViewTopicActivity.java
@@ -1,17 +1,25 @@
 package kr.co.tjoeun.colosseum_kotlin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import kr.co.tjoeun.colosseum_kotlin.databinding.ActivityViewTopicBinding;
+import kr.co.tjoeun.colosseum_kotlin.utils.ServerUtil;
 
 public class ViewTopicActivity extends BaseActivity {
 
-
+    ActivityViewTopicBinding binding ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_topic);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_view_topic);
         setupEvents();
         setValues();
     }
@@ -24,5 +32,20 @@ public class ViewTopicActivity extends BaseActivity {
     @Override
     public void setValues() {
 
+        int topicId = getIntent().getIntExtra("topic_id", -1);
+
+        if(topicId ==-1){
+//            TODO - 주제가 제대로 못넘어왔다
+            Toast.makeText(mContext, "잘못된 접근입니다.", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        ServerUtil.getRequestTopicById(mContext, topicId, new ServerUtil.JsonResponseHandler() {
+            @Override
+            public void onResponse(JSONObject json) {
+                Log.d("토픽 상세 정보", json.toString());
+            }
+        });
+        
     }
 }
